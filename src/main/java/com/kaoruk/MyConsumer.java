5e1b255115bea5c6509989265a0c843b7fb45094
Kaoru Kohashigawa <dev@kaoruk.com>
2017-01-07 15:41:12 -0800

    remove lock and replace it with atomic boolean so that we don't lock a thread when making a request to the broker

--- src/main/java/com/kaoruk/MyConsumer.java
+++ src/main/java/com/kaoruk/MyConsumer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.errors.WakeupException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,21 +14,20 @@ import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by kaoru on 1/6/17.
  */
 public class MyConsumer implements Runnable{
     private KafkaConsumer<String, String> consumer;
-    private volatile boolean running = true;
-    private ReentrantLock lock;
+    private AtomicBoolean running = new AtomicBoolean(true);
     private String name;
 
-    public MyConsumer(String name, ReentrantLock lock) {
+    public MyConsumer(String name) {
         name = Optional.fromNullable(name).or("default");
         this.name = name;
-        this.lock = lock;
 
         consumer = new KafkaConsumer<String, String>(getProps());
         consumer.subscribe(Arrays.asList(MyTwitter.TOPIC_NAME));
@@ -38,76 +38,73 @@ public class MyConsumer implements Runnable{
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put("group.id", "TwitterConsumptionB");
-        props.put("enable.auto.commit", "false");
+        props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
 //        props.put("session.timeout.ms", "10000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 1_000);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, (int) TimeUnit.SECONDS.toMillis(1));
 
         return props;
     }
 
     public void shutdown() {
-        try {
-            lock.lock();
-            running = false;
-            consumer.close();
-            System.out.println("SHUTTING DOWN CONSUMER " + this.name);
-        } finally {
-            lock.unlock();
-        }
+        running.set(false);
+        consumer.wakeup();
     }
 
     @Override
     public void run() {
         System.out.println(this.name + " is running!");
-        while (running) {
-            ConsumerRecords<String, String> records;
+        ConsumerRecords<String, String> records;
 
-            try {
-                lock.lock();
-                if (running) {
-                    records = consumer.poll(100);
-                } else {
-                    break;
-                }
-            } finally {
-                lock.unlock();
-            }
+        try {
+            while (running.get()) {
+                records = consumer.poll(100);
 
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println(this.name +
-                        color(" partition: " + record.partition(), ThreadColor.ANSI_PURPLE) +
-                        color(" offset: " + record.offset(), ThreadColor.ANSI_BLUE ) +
-                        " timestamp: " + record.timestamp()
-                );
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(this.name +
+                            color(" partition: " + record.partition(), ThreadColor.ANSI_PURPLE) +
+                            color(" offset: " + record.offset(), ThreadColor.ANSI_BLUE) +
+                            " timestamp: " + record.timestamp()
+                    );
+
 //                System.out.println(this.name.toUpperCase() +
 //                        " offset: " + record.offset() +
 //                        " value: " + record.value()
 //                );
+                }
             }
-
-            consumer.commitAsync();
+        } catch (WakeupException e) {
+            System.out.println("Wake up called, SHUTTING DOWN CONSUMER " + this.name);
+        } finally {
+            consumer.close();
+            System.out.println(this.name + " STOPPED CONSUMMING!");
         }
-        System.out.println(this.name + " STOPPED CONSUMMING!");
     }
 
     public static void main(String[] args) {
         ExecutorService pool = Executors.newFixedThreadPool(10);
-        ReentrantLock lock = new ReentrantLock();
-
         List<MyConsumer> consumers = new ArrayList<>();
 
-        consumers.add(new MyConsumer(color("Consumer_1", ThreadColor.ANSI_RED), lock));
-        consumers.add(new MyConsumer(color("Consumer_2", ThreadColor.ANSI_GREEN), lock));
-        consumers.add(new MyConsumer(color("Consumer_3", ThreadColor.ANSI_CYAN), lock));
-        consumers.add(new MyConsumer(color("Consumer_4", ThreadColor.ANSI_YELLOW), lock));
-        consumers.add(new MyConsumer(color("Consumer_5", ThreadColor.ANSI_BLUE), lock));
-        consumers.add(new MyConsumer(color("Consumer_6", ThreadColor.ANSI_PURPLE), lock));
+        consumers.add(new MyConsumer(color("Consumer_1", ThreadColor.ANSI_RED)));
+        consumers.add(new MyConsumer(color("Consumer_2", ThreadColor.ANSI_GREEN)));
+        consumers.add(new MyConsumer(color("Consumer_3", ThreadColor.ANSI_CYAN)));
+        consumers.add(new MyConsumer(color("Consumer_4", ThreadColor.ANSI_YELLOW)));
+        consumers.add(new MyConsumer(color("Consumer_5", ThreadColor.ANSI_BLUE)));
+        consumers.add(new MyConsumer(color("Consumer_6", ThreadColor.ANSI_PURPLE)));
 
         consumers.forEach(pool::execute);
+
+        try {
+            Thread.sleep(TimeUnit.MINUTES.toMillis(5));
+        } catch (InterruptedException e) {
+            System.out.println("Sleep was interrupted for whatever reason");
+        }
+
+        consumers.forEach(MyConsumer::shutdown);
+        pool.shutdown();
     }
 
     private static String color(String text, String color) {
